9a033cb4c784118ba7d6e71949cb0173c1ce6e46
Jesse Donat <donatj@gmail.com>
2012-05-17 20:50:41 -0500

    Massive Improvment

---
+++ range-polyfill.js
@@ -0,0 +1,29 @@
+
+window.addEvent('domready', function(){
+	if( new Element('input[type=range]').type == 'text') {
+		$$('input[type=range]').each(function(e){
+			var min  = parseFloat(e.get('min' ))  || 0;
+			var max  = parseFloat(e.get('max' ))  || 100;
+			var step = parseFloat(e.get('step')) || 1;
+			var scale = parseFloat(e.get('data-poly-scale')) || 1;
+			var selected = e.get('value') || min;
+
+			var select = new Element('select', {
+				'id'   : e.get('id'),
+				'class': e.get('class'),
+			});
+			select.cloneEvents(e);
+
+			for(var i = min; i <= max; i += step ) {
+				var option = new Element('option', {
+					'html'    : ((i * scale) + "").substring(0,3), //JavaScript sucks at numbers
+					'value'   : i,
+					'selected': i == selected ? 'selected' : '',
+				});
+				select.adopt(option);
+			}
+
+			select.replaces(e);
+		});
+	}
+});
