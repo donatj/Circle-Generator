94d37d4208ed4545315f2427ba9fff90ad891464
Jesse Donat <donatj@gmail.com>
2026-08-19 15:58:09 -0500

    Declare Vite build requirements

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+	base: './',
+	build: {
+		outDir: 'dist',
+	},
 	plugins: [viteSingleFile({ removeViteModuleLoader: true })],
 });
